c2923f4bc52b323dd0f5a10a76e6bd547f00b161
deCODEyn <decodyntech@gmail.com>
2025-07-18 20:20:59 -0300

    Implemented answer finder

--- src/http/routes/create-question.ts
+++ src/http/routes/create-question.ts
@@ -1,7 +1,9 @@
+import { and, eq, sql } from 'drizzle-orm';
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
 import { z } from 'zod';
 import { db } from '../../db/connection.ts';
 import { schema } from '../../db/schema/index.ts';
+import { generateAnswer, generateEmbeddings } from '../../services/gemini.ts';
 
 export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -20,12 +22,38 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
       const { idRoom } = request.params;
       const { question } = request.body;
 
+      const embeddings = await generateEmbeddings(question);
+      const embeddingsAsString = `[${embeddings.join(',')}]`;
+
+      const chuncks = await db
+        .select({
+          id: schema.audioChunks.id,
+          transcription: schema.audioChunks.transcription,
+          similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`,
+        })
+        .from(schema.audioChunks)
+        .where(
+          and(
+            eq(schema.audioChunks.idRoom, idRoom),
+            sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
+          )
+        )
+        .orderBy(
+          sql`${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`
+        )
+        .limit(3);
+
+      let answer: string | null = null;
+
+      if (chuncks.length > 0) {
+        const transcriptions = chuncks.map((chunck) => chunck.transcription);
+
+        answer = await generateAnswer(question, transcriptions);
+      }
+
       const result = await db
         .insert(schema.questions)
-        .values({
-          idRoom,
-          question,
-        })
+        .values({ idRoom, question, answer })
         .returning();
 
       const insertedQuestion = result[0];
@@ -34,7 +62,9 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
         throw new Error('Failed to create new Question.');
       }
 
-      return reply.status(201).send({ idQuestion: insertedQuestion.id });
+      return reply
+        .status(201)
+        .send({ idQuestion: insertedQuestion.id, answer });
     }
   );
 };
